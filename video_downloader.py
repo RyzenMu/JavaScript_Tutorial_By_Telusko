7fd94f2235af99ffcf43c286b40a85b1450db9f1
RyzenMu <muruga13in2022@gmail.com>
2022-10-11 21:35:56 +0530

    links identified

--- video_downloader.py
+++ video_downloader.py
@@ -1,11 +1,30 @@
+from ast import pattern
 import requests
 from bs4 import BeautifulSoup
+import re
 
 yt_site = requests.get('https://www.youtube.com/watch?v=PlbupGCBV6w&list=PLsyeobzWxl7rrvgG7MLNIMSTzVCDZZcT4').text
 
 
 soup = BeautifulSoup(yt_site, "lxml")
 
-anchor_tags = soup.find("ytd-playlist-panel-video-renderer")
+# print(soup.prettify()) 
 
-print(anchor_tags)
+# pattern = 'https://\S+'
+
+# pattern = 'Telusko'
+
+# pattern = 'www.youtube.com/watch\S+'
+
+# pattern = 'href="watch\S+'
+
+pattern = 'index\S+'
+
+results = re.findall(pattern, str(soup.prettify()))
+
+
+for result in results:
+    print()
+    print(result)
+    print('------------------')
+    print()
