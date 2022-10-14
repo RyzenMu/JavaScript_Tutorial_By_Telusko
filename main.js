33f9663b9bcac8ac5b2a8a25c208ede25ad0e60f
RyzenMu <muruga13in2022@gmail.com>
2022-10-14 20:52:47 +0530

    document not working in node js

--- main.js
+++ main.js
@@ -21,4 +21,29 @@ for (let i = 0; i <= 0; i++)
 
 
 
+var fs = require('fs')
+// const { text } = require('stream/consumers')
+var files = fs.readdirSync('/media/a/719c6d20-e2dd-4446-a15a-421ae8936f12/home/ubuntu/Desktop/JavaScript_Tutorial_By_Telusko/')
+
+
+for (let i = 0; i<files.length; i++)
+{
+    try
+    {
+        let video = document.createElement('video')
+        video.src = files[i]
+        let box = document.getElementById('new')
+        box.appendChild(video)
+        video.controls = true
+        console.log(video.src)
+        console.log(files[i])
+
+    }
+    catch(err)
+    {
+        console.log('error found', i)
+    }
+}
+
+document.write(files)
 
--- main_node.js
+++ main_node.js
@@ -5,5 +5,28 @@
 // console.log('Ryzen Cpu details : ', os.cpus())
 
 var fs = require('fs')
+// const { text } = require('stream/consumers')
 var files = fs.readdirSync('/media/a/719c6d20-e2dd-4446-a15a-421ae8936f12/home/ubuntu/Desktop/JavaScript_Tutorial_By_Telusko/')
 console.log(files)
+
+for (let i = 0; i<files.length; i++)
+{
+    try
+    {
+        // let video = document.createElement('video')
+        // video.src = files[i]
+        // let box = document.getElementById('new')
+        // box.appendChild(video)
+        // video.controls = true
+        // console.log(video.src)
+        console.log(files[i])
+
+    }
+    catch(err)
+    {
+        console.log('error found', i)
+    }
+}
+
+
+
