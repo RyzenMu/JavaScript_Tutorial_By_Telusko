76229df0daea5f716d5844523b4f54d1ab251d86
RyzenMu <muruga13in2022@gmail.com>
2022-10-18 21:58:21 +0530

    thumbnail added to videos

--- thumnail_generator/thumbnail_generator.py
+++ thumnail_generator/thumbnail_generator.py
@@ -10,9 +10,10 @@ list_dir = os.listdir()
 
 for filename in list_dir:
     if 'mp4' in filename:
-        print(filename)
+        filename_print = filename[0:25]
+        print(filename_print)
         turtle.color('black')
-        turtle.write(filename, font=('verdana', 100, 'normal'), align='center')
+        turtle.write(filename_print, font=('verdana', 50, 'normal'), align='center')
 
         ts = turtle.getscreen()
         # ts.getcanvas().postscript(file="0-intoduction.eps")
@@ -25,6 +26,7 @@ for filename in list_dir:
         rgb_image = image.convert('RGB')
 
         rgb_image.save(filename+'.png')
+        turtle.clearscreen()
 
 
 
